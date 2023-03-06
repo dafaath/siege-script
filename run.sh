0e3d142484de8d8b1ba9f71a9bea72f54fedd368
dapaath <dafaathaullah123@gmail.com>
2023-03-07 03:12:06 +0700

    modifying script

--- run.sh
+++ run.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
-HOST=10.104.0.2
-PORT=3000
+TYPE=$1
+
+if [ "$TYPE" == "dafav1" ] || [ "$TYPE" == "dafav2" ]; then
+    HOST=localhost
+    PORT=3000
+    AUTH_METHOD=jwt
+elif [ "$TYPE" == "alvinv1" ] || [ "$TYPE" == "alvinv2" ]; then
+    HOST=10.104.0.5
+    PORT=5000
+    AUTH_METHOD=jwt
+elif [ "$TYPE" == "hanin" ]; then
+    HOST=10.104.0.4
+    PORT=5000
+    AUTH_METHOD=basic
+elif [ "$TYPE" == "" ]; then
+    echo "Missing parameter, usage ./run.sh [dafav1|dafav2|alvinv1|alvinv2|hanin]"
+    exit 1
+else
+    echo "Invalid parameter $TYPE, must be dafav1, dafav2, alvinv1, alvinv2, or hanin"
+    exit 1
+fi
+
 TEST_TIME=60s
 SLEEP_TIME=120
 TIMEOUT=600 # 10 minutes
 ITERATION=5
 CONCURENCY=(200 400 600 800 1000)
-AUTH_METHOD=jwt
 USERNAME="perftest"
 PASSWORD="perftest"
 
@@ -34,9 +53,27 @@ rollback_db() {
     init_db
 }
 
-perftest() {
+test_connection() {
     local method=$1
     local endpoint=$2
+    local data=$3
+    local do_rollback=$4
+    if [ $method == "GET" ] || [ $method == "DELETE" ]; then
+        local STATUS="$(curl -s -o /dev/null -w "%{http_code}" "$HOST:$PORT$endpoint" --header "$HEADER" | xargs)"
+        local EXIT_CODE=$?
+    else
+        local STATUS="$(curl -s -o /dev/null -w "%{http_code}" "$HOST:$PORT$endpoint $method $data" | xargs)"
+        local EXIT_CODE=$?
+    fi
+
+    if [ "$STATUS" -ge "400" ] || [[ $EXIT_CODE -ne 0 ]]; then
+        echo "Connection test failed, status code $STATUS for $HOST:$PORT$endpoint $method $data"
+        exit 1
+    fi
+}
+
+perftest() {
+    local method=$1 local endpoint=$2
     local data=$3
     local do_rollback=$4
     for concurrent in "${CONCURENCY[@]}"; do
@@ -86,6 +123,23 @@ auth() {
 
 rollback_db
 auth
+
+echo "Testing connection..."
+test_connection "GET" "/node" "" false
+test_connection "GET" "/node/1" "" false
+if [ "$TYPE" == "alvinv2" ] && [ "$TYPE" == "dafav2" ]; then
+    # ! TODO CHANGE PAYLOAD
+    test_connection "PUT" "/node/1" "{ \"name\":\"test\",\"location\":\"test\",\"id_hardware\":1 }" true
+    test_connection "POST" "/node" "{ \"name\":\"test\",\"location\":\"test\",\"id_hardware\":1 }" true
+    test_connection "POST" "/channel" "{\"value\": 1.33, \"id_sensor\": 1}" true
+else
+    test_connection "GET" "/sensor" "" false
+    test_connection "GET" "/sensor/1" "" false
+    test_connection "PUT" "/node/1" "{ \"name\":\"test\",\"location\":\"test\",\"id_hardware\":1 }" true
+    test_connection "POST" "/node" "{ \"name\":\"test\",\"location\":\"test\",\"id_hardware\":1 }" true
+    test_connection "POST" "/channel" "{\"value\": 1.33, \"id_sensor\": 1}" true
+fi
+echo "Connection test success"
 
 ## perftest METHOD ENDPOINT DATA DO_ROLLBACK
 perftest "GET" "/node" "" false
